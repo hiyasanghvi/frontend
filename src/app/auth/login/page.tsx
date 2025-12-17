db0147ed209818b0bc54acd76d89c5aea976144e
Hiya Sanghvi <169714000+hiyasanghvi@users.noreply.github.com>
2025-12-18 01:07:47 +0530

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -16,11 +16,11 @@ export default function LoginPage() {
     setMessage("");
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
-      });
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(form),
+});
 
       const data = await res.json();
 
