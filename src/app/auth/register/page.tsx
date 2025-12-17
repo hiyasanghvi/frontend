161bf92415a9208948890be14751b5480a05a5da
Hiya Sanghvi <169714000+hiyasanghvi@users.noreply.github.com>
2025-12-18 01:06:25 +0530

    Update page.tsx

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -22,11 +22,13 @@ export default function RegisterPage() {
     setMessage("");
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/auth/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
-      });
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/register`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(form),
+});
+
+    
 
       const data = await res.json();
 
